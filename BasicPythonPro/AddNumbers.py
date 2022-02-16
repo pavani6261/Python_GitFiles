c6279ae5d0debd2acb2e9fca15415e2762729411
Thummana.pavani <pavani6261>
2022-02-16 10:46:31 +0530

    first commit of this file

---
+++ BasicPythonPro/AddNumbers.py
@@ -0,0 +1,13 @@
+n1=7
+n2= 3
+print("arithematic operations:")
+print("add n1 + n2=", end=' ')
+print(n1 + n2)
+print("add n1 - n2=", end=' ')
+print(n1 - n2)
+print("add n1 * n2=", end=' ')
+print(n1 * n2)
+print("add n1 / n2=", end=' ')
+print(n1 / n2)
+print("add n1 % n2=", end=' ')
+print(n1 % n2)
