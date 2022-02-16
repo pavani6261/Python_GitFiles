66d40d561857c46f5b08fb296b0b072af8222c47
Thummana.pavani <pavani6261>
2022-02-16 11:38:42 +0530

    1st commit to this program

---
+++ BasicPythonPro/Display.py
@@ -0,0 +1,39 @@
+# ch=[2,3,4,5,6]
+# for i in (ch):
+#     print(i, end= ' ')
+# print()
+
+# str ="Hello"
+# name = "sam"
+# print(str)
+# print(str + ' ' + "raj") # concatination
+# print(str)
+# # print(str + ' ' + name)
+# str = str+ ' ' +name #  string concatination and storing in to variable
+# print(str)
+# print(str[0:7])
+# print(str[0:4] +' '+ str[6:8]) # slicing
+# print(len(str))  #  print length of string
+#
+# for i in range(0,len(str)):  #print the element untill the end o stirng
+#     print(str[i],end= ' ')
+#
+
+# k=1    #printing numbers in a matrix format
+# for i in range(4):
+#     for j in range(4):
+#         print(k,end = '\t')
+#         k=k+1
+#     print()
+
+## print the word
+for i in "World":
+    print(i, end = " ")
+print()
+
+## print triangle using *
+for i in range(3):
+    for j in range(0,i+1):
+        print('*',end= ' ')
+    print()
+
