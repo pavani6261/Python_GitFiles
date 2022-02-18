d48678b988f38507004752b99123f91495947ec3
Thummana.pavani <pavani6261>
2022-02-18 09:21:34 +0530

    1st commit to this program

---
+++ BasicPythonPro/TestingPython.py
@@ -0,0 +1,11 @@
+# #using pytest
+# b=-7
+# def test_numbers():
+#     assert b>=0   #output is failed #if b is number =0 or >0 then output will be passed
+
+# # pytest using decorator
+# import pytest
+# @pytest.mark.parametrize("num,result",[(1,11),(2,22),(4,44),(3,23),(5,55)])
+# def test_mul(num,result):
+#     assert 11*num == result
+#
